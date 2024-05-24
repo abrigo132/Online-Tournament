14665a9e36c9a9aa770d64699d60913192f0fafb
abrigo132 <garipotter97@gmail.com>
2024-05-24 15:02:04 +0500

    Выделение логики регистрации пользователя в отдельную функцию, от которой потом зависит вью

--- src/auth/register_view.py
+++ src/auth/register_view.py
@@ -1,14 +1,25 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Form, HTTPException
+from starlette import status
 
-from src.auth.schemas import GamersRegister
-from src.database import session
 from src.crud.crud import insert_gamer_db
+from src.auth.schemas import GamersRegister
 
 router = APIRouter(prefix="/register", tags=["register"])
 
 
+async def check_register_user(username: str = Form(), password: str = Form(), steam_id: str = Form(),
+                              email: str = Form(), age: int = Form(), ):
+    db_response: dict = await insert_gamer_db(username, password, steam_id, email, age)
+    if db_response["status"] == "bad":
+        raise HTTPException(
+            status_code=status.HTTP_408_REQUEST_TIMEOUT,
+            detail="К сожалению, произошла ошибка. Попробуйте позднее"
+        )
+    return username
+
+
 @router.post("/gamers")
-async def response_register_gamers(data: GamersRegister = Depends(insert_gamer_db)):
+async def response_register_gamers(username: GamersRegister = Depends(check_register_user)):
     return {
-        "message": f"Поздравляю,{data.username}! Вы зарегистрировались, как игрок! Успехов на турнирах",
+        "message": f"Поздравляю,{username}! Вы зарегистрировались, как игрок! Успехов на турнирах",
     }
