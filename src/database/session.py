2de89c9b4303953ba107562329f86bb4530d3327
abrigo132 <garipotter97@gmail.com>
2024-05-24 13:55:12 +0500

    добавление комментариев

--- src/database/session.py
+++ src/database/session.py
@@ -3,11 +3,24 @@ from sqlalchemy.orm import DeclarativeBase
 from config import settings
 
 engine = create_async_engine(url=settings.db.DATABASE_URL_asyncpg,
-                                          echo=True,
-                                          )
+                             echo=True,
+                             )
 
 session = async_sessionmaker(engine)
 
 
 class Base(DeclarativeBase):
-    pass
+    repr_cols_num = 3
+    repr_cols = tuple()
+    """
+    Магический метод __repr__ используется для красивого отображения вывода ответа алхимии
+    
+    """
+    def __repr__(self):
+        """Relationships не используются в repr(), т.к. могут вести к неожиданным подгрузкам"""
+        cols = []
+        for idx, col in enumerate(self.__table__.columns.keys()):
+            if col in self.repr_cols or idx < self.repr_cols_num:
+                cols.append(f"{col}={getattr(self, col)}")
+
+        return f"<{self.__class__.__name__} {', '.join(cols)}>"
