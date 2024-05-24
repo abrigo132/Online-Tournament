848ac894fa726510662db527e9f95a30b912eb03
abrigo132 <garipotter97@gmail.com>
2024-05-24 14:53:54 +0500

    Изменение логики в регистрации пользователей

--- src/crud/crud.py
+++ src/crud/crud.py
@@ -1,24 +1,33 @@
+from asyncpg import ConnectionDoesNotExistError
 from sqlalchemy import select
 
-from src.auth.schemas import GamersRegister, GamersAuth
+from src.auth.schemas import GamersRegister
 from src.database.session import session
 from src.database.models import Gamers
 from src.auth.utils_jwt import hash_password
 from src.database.schemasDTO import GamersGetDTO
 
 
-async def insert_gamer_db(data_user: GamersRegister) -> None:
-    new_gamer = Gamers(username=data_user.username,
-                       password=hash_password(data_user.password),
-                       steam_id=data_user.steam_id,
-                       email=data_user.email,
-                       age=data_user.age,
-                       status="active")
-    async with session() as data_db:
-        gamer = data_db.add(new_gamer)
-        await data_db.commit()
+async def insert_gamer_db(username: str, password: str, steam_id: str, email: str, age: int,
+                          status: str = "active") -> None:
+    new_gamer = Gamers(username=username,
+                       password=hash_password(password),
+                       steam_id=steam_id,
+                       email=email,
+                       age=age,
+                       status=status)
+    try:
+        async with session() as data_db:
+            gamer = data_db.add(new_gamer)
+            await data_db.commit()
+    except ConnectionDoesNotExistError:
+        return {
+            "status": "bad"
+        }
 
-    return data_user
+    return {
+        "status": "ok"
+    }
 
 
 async def check_user(username: str) -> None:
