b608cfe4a65cadfc27c932360a0e0d27efb27240
abrigo132 <garipotter97@gmail.com>
2024-05-24 13:55:31 +0500

    добавление комментариев

--- src/database/schemasDTO.py
+++ src/database/schemasDTO.py
@@ -2,9 +2,17 @@ from pydantic import BaseModel
 
 
 class GamersPostDTO(BaseModel):
+    """
+    Модель для преобразования ответа sqlalchemy в pydantic модель Data transfer object
+
+    """
     id: int
 
 
 class GamersGetDTO(GamersPostDTO):
+    """
+    Модель для преобразования ответа sqlalchemy в pydantic модель Data transfer object
+
+    """
     username: str
-    hashed_password: bytes
+    password: bytes
