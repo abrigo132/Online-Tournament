81c3cb9c2ca8e9ccb4940d1bd85986dbaf1f9b6d
abrigo132 <garipotter97@gmail.com>
2024-06-07 14:29:02 +0500

    add gamer_router

--- src/main.py
+++ src/main.py
@@ -4,12 +4,14 @@ import uvicorn
 from src.auth.register_view import router as register_router
 from src.auth.auth_view import router as auth_router
 from src.tournament_view.fastapi_view import router as tournament_router
+from src.gamers_view.views import router as gamer_router
 app = FastAPI()
 
 
 app.include_router(register_router)
 app.include_router(auth_router)
 app.include_router(tournament_router)
+app.include_router(gamer_router)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
