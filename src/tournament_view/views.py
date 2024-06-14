cce2ab38f152a1ced8c4bb7e5dd2828fae44c840
abrigo132 <garipotter97@gmail.com>
2024-06-14 11:49:23 +0500

    add tournament info and register squad on tournament

--- src/tournament_view/views.py
+++ src/tournament_view/views.py
@@ -2,7 +2,8 @@ from fastapi import APIRouter, Depends, HTTPException
 from starlette import status
 
 from src.tournament_view.schemas import NewTournament, TournamentInfo
-from src.crud.crud import insert_tournament_into_db, check_tournament_info
+from src.crud.crud import (insert_tournament_into_db, check_tournament_info,
+                           register_squad_on_tournament_and_add_tournament_in_squad, info_participants_in_tournament)
 
 router = APIRouter(prefix="/tournament", tags=["tournament"])
 
@@ -28,14 +29,42 @@ def add_new_tournament(tournament_name: str = Depends(check_register_new_tournam
 async def about_tournament(tournament_data: TournamentInfo):
     response_db = await check_tournament_info(tournament_data.name)
     return response_db
-    
+
+
+async def about_squads_in_tournament(tournament_name: str):
+    squad_list = await info_participants_in_tournament(tournament_name)
+    return squad_list
+
 
 @router.post("/info/{tournament_name}/")
-def get_tournament_by_id(tournament_info: TournamentInfo = Depends(about_tournament)):
+def get_tournament_by_id(squads_info: list = Depends(about_squads_in_tournament)):
+    squads_in_tournaments = []
+    for value in squads_info[0].squads_list:
+        squads_in_tournaments.append(value.squad_name)
     return {
-        "name": tournament_info[0].tournament_name,
-        "teams": tournament_info[0].teams,
-        "type": tournament_info[0].tournament_type,
-        "created_at": tournament_info[0].created_at,
-        "finished_at": tournament_info[0].finished_at
+        "name": squads_info[0].tournament_name,
+        "teams": squads_info[0].number_of_teams,
+        "type": squads_info[0].tournament_type,
+        "created_at": squads_info[0].created_at,
+        "finished_at": squads_info[0].finished_at,
+        "participants": squads_in_tournaments
+
     }
+
+
+@router.post("/register/squad/")
+async def register_squad_on_tournament(tournament_name: str, squad_name: str) -> dict:
+    add_squad_on_tournament: dict[str, str] = await register_squad_on_tournament_and_add_tournament_in_squad(
+        tournament_name=tournament_name,
+        squad_name=squad_name)
+    if add_squad_on_tournament.get("status") == "bad":
+        return {
+            HTTPException(
+                status_code=status.HTTP_408_REQUEST_TIMEOUT,
+                detail="Произошла ошибка, попробуйте позднее"
+            )
+        }
+    else:
+        return {
+            "message": f"Поздравляю, ваша команда {squad_name} зарегистрировалась на турнир {tournament_name}. Успехов!"
+        }
