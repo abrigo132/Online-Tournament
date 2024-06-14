7aa50f07e74893c0fe06b1cfccef91843072ae92
abrigo132 <garipotter97@gmail.com>
2024-06-14 21:43:52 +0500

    add squad info for gamer

--- src/squads_view/views.py
+++ src/squads_view/views.py
@@ -3,7 +3,7 @@ from typing import Any
 from fastapi import APIRouter, HTTPException, Depends
 from starlette import status
 
-from src.crud.crud import create_squad, info_squads_with_tournaments
+from src.crud.crud import create_squad, info_squads_with_tournaments, add_gamer_into_squad
 from src.squads_view.schemas import NewSquad
 from src.database.models import Squads
 
@@ -33,11 +33,27 @@ async def check_info_squad(squad_name: str) -> list[Squads]:
 @router.post("/info/{squad_name}")
 async def squad_info(squad_name: list[Any] = Depends(check_info_squad)):
     tournaments = []
+    gamers = []
     for value in squad_name[0].tournaments_list:
         tournaments.append(value.tournament_name)
+    for value in squad_name[0].gamers:
+        gamers.append(value.username)
     return {
         "squad_name": squad_name[0].squad_name,
         "about": squad_name[0].about_of_team,
         "created_at": squad_name[0].created_at,
-        "tournaments": tournaments
+        "members": gamers,
+        "tournaments": tournaments,
+    }
+
+
+async def check_add_gamer_into_squad(gamer_name: str, squad_name: str):
+    result_db = await add_gamer_into_squad(gamer_name=gamer_name, squad_name=squad_name)
+    return gamer_name, squad_name
+
+
+@router.post("/add/gamer/squad")
+def add_gamer_squad(check_info=Depends(check_add_gamer_into_squad)):
+    return {
+        "message": f"Поздравляю {check_info[0]}, вы вступили в команду: {check_info[1]}"
     }
